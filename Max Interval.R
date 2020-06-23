73050f307aa076f545f493f985dfb0b645a7ba9e
ked66 <ked66@cornell.edu>
2020-06-23 12:22:44 -0400

    Initial calculation of mean steps per 5-minute interval
    Initial plot of mean steps per 5-minute interval
    
    Initial calculation of max steps per 5-minute interval

---
+++ Max Interval.R
@@ -0,0 +1,26 @@
+## Make a time series plot (i.e. type = "l") of the 5-minute interval (x-axis) and 
+## the average number of steps taken, averaged across all days (y-axis)
+library(data.table)
+by_interval <- tapply(complete_data$steps, complete_data$interval, mean) %>%
+  as.data.frame() %>%
+  setDT(keep.rownames = TRUE)
+names(by_interval) <- c("interval", "steps")
+by_interval <- mutate(by_interval, interval = as.numeric(interval))
+
+plot(by_interval$interval, by_interval$steps, 
+     type = "l", col = "maroon",
+     main = "Mean Number of Steps per 5-minute Interval",
+     xlab = "5-Minute Interval",
+     ylab = "Mean Number of Steps")
+
+library(ggplot2)
+ggplot(data = by_interval, aes(interval, steps, group = 1)) + geom_line(col = "maroon") +
+  labs(title = "Mean Number of Steps per 5-minute Interval", x = "5-Minute Interval",
+       y = "Mean Number of Steps")
+
+## Which 5-minute interval, on average across all the days in the dataset, 
+## contains the maximum number of steps?
+max <- max(by_interval$steps)
+max_interval <- subset(by_interval, steps == max)
+max_interval  ## Interval 835
+
