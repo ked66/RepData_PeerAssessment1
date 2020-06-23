b72f3976d4788ff751ae5b92cb90652ecd063824
ked66 <ked66@cornell.edu>
2020-06-23 11:53:56 -0400

    Initial loading/processing of activity data

---
+++ Processing Code.R
@@ -0,0 +1,9 @@
+## Unzip data and read csv file
+data <- read.csv(unz("activity.zip", "activity.csv"))
+str(data)
+
+## Convert Date variable to POSIX.ct and Interval variable to factor
+library(dplyr)
+data <- mutate(data, date = as.POSIXct(date, tz = "", "%Y-%m-%d")) %>%
+  mutate(interval = as.factor(interval))
+  
